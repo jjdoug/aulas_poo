dc8139e37cce849c972c53d912d2aee18ba24100
Jefferson Douglas <jedouglas.study@gmail.com>
2024-11-03 18:39:07 -0300

    Update Principal.java

--- exercicio_poo05/pack02/Principal.java
+++ exercicio_poo05/pack02/Principal.java
@@ -4,12 +4,29 @@ import exercicio_poo05.pack01.Pessoa;
 
 public class Principal {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa();
-        p1.imprimirDados("Jefferson", 2001, 1.77f);
+        
+        Pessoa p1 = new Pessoa("Jefferson", 2001, 1.77f);
+        p1.imprimirDados();
         p1.calcularIdade();
         
-        Pessoa p2 = new Pessoa();
-        p2.imprimirDados("Ricson Santana", 1982, 1.77f);
+        Pessoa p2 = new Pessoa("Ricson Santana", 1982, 1.77f);
+        p2.imprimirDados();
         p2.calcularIdade();
+        
+        System.out.println("*Dados do maior*");
+        if (p1.getAltura() > p2.getAltura()){
+            p1.imprimirDados();
+        }
+        else {
+            p2.imprimirDados();
+        }
+        
+        System.out.println("*Dados do mais velho*");
+        if (p1.getAno_nasc() < p2.getAno_nasc()){
+            p1.imprimirDados();
+        }
+        else {
+            p2.imprimirDados();
+        }
     }
 }
