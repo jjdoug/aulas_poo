0ca8e80496eec85a56507092bb02296c96033f26
Jefferson Douglas <jedouglas.study@gmail.com>
2024-11-03 18:39:47 -0300

    Update Pessoa.java

--- exercicio_poo05/pack01/Pessoa.java
+++ exercicio_poo05/pack01/Pessoa.java
@@ -1,20 +1,48 @@
 package exercicio_poo05.pack01;
 
 public class Pessoa {
+    //Atributos
     private String nome;
-    private int ano_nasc;
     private float altura;
+    private int ano_nasc;
     
-    public void imprimirDados(String nome, int ano_nasc, float altura){
+    //Métodos especiais
+    public Pessoa(String nome, int ano_nasc, float altura){
         this.nome = nome;
         this.ano_nasc = ano_nasc;
         this.altura = altura;
-        System.out.println("Nome: "+ this.nome);
-        System.out.println("Ano de nascimento: "+this.ano_nasc);
-        System.out.println("Altura: "+this.altura);
     }
+    
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public int getAno_nasc() {
+        return ano_nasc;
+    }
+    public void setAno_nasc(int ano_nasc) {
+        this.ano_nasc = ano_nasc;
+    }
+    
+    //Métodos
+    public void imprimirDados(){
+        System.out.println("Nome: " + getNome());
+        System.out.println("Ano de nascimento: " + getAno_nasc());
+        System.out.println("Altura: " + getAltura());
+    }
+    
     public void calcularIdade(){
-        int idade = 2024-ano_nasc;
-        System.out.println("Idade: "+idade);
+        int idade = 2024-getAno_nasc();
+        System.out.println("Idade: " + idade);
     }
 }
